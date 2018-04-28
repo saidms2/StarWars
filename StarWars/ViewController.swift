e68010259c9b5bab7a7a5f58962cd68ccb3f1603
said <saidms2@gmail.com>
2018-04-28 13:14:55 -0400

    add print
    viewcontroller

--- StarWars/ViewController.swift
+++ StarWars/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
         }
         
         let person = Person(name: "jean", height:188)
-        //let person2 = Person("jean")
+        let person2 = Person("jean")
 
         //print (person)
         print (person.getName())
@@ -49,7 +49,9 @@ class ViewController: UIViewController {
         heightLabel.text = "\(person.height)"
         hairColorLabel.text = person.hairColor.rawValue
         
-            }
+        print (person2.getName())
+        
+        }
     
 }
 
